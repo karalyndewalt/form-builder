f104d6170077337cbd63b796f1e689e1de62c397
KaraLyn DeWalt <karalyn.dewalt@gmail.com>
2018-02-13 21:33:31 -0800

    Adds Form styling, conditional Condition in Form, moves most react components to functions(may change), redistributes AnswerType to Presentation and Container components (trying for fun),

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React  from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, combineReducers } from 'redux';
-import { FormGroup, FormControl, ControlLabel,
+import { Form, FormGroup, FormControl, ControlLabel, Col,
         PageHeader,
-        Button, ButtonToolbar, } from 'react-bootstrap';
+        Button, ButtonToolbar,
+        Well, } from 'react-bootstrap';
 // import 'bootstrap/dist/css/bootstrap.css';
 // import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
@@ -19,6 +20,7 @@ const questions = (state = [], action) => {
           id: qId++,
           text: action.text,
           answerType: action.answerType,
+          condition: action.condition,
           // sub-input = [],
         }
       ]
@@ -52,11 +54,10 @@ const questions = (state = [], action) => {
 //       // look for id to del, if ?  !id return q : not empty ....
     default:
       return state;
-//
   }
 };
 
-// visibilityFilter will set the view based on the Nav selected
+// view will set the view based on the Nav selected
 const view = (
   state = "CREATE_FORMS",
   action
@@ -80,110 +81,176 @@ const store = createStore(formBuilder);
 
 
 //////////// REACT COMPONENTS ///////////////
-class Question extends React.Component {
 
-  render () {
-    // console.log(store.getState());
-    return (
+const Condition = ({
+  id,
 
-      <div>
-        <form>
-          {/*  To inline lable with input field -->
-            https://react-bootstrap.github.io/components/forms/#forms-horizontal*/}
+}) => (
+  <Form inline >
+    <FormGroup controlId={"condition-" + id} >
+      <ControlLabel>Condition</ControlLabel>{' '}
+        <FormControl
+          componentClass="select"
+          // inputRef={ref => { this.input = ref; }}
+          // onChange={() => {
+          //   store.dispatch({
+          //     type: "CHANGE_TYPE",
+          //     id: this.props.id,
+          //     answerType: this.input.value, //**TODO** FIXME, needs selected option value **use event handler
+          //   })
+          // }}
+        >
+          <option value="equal">Equals</option>
+          <option value="greater">Greater than</option>
+          <option value="less">Less than</option>
+        </FormControl>
+    </FormGroup>{" "}
+    <FormGroup controlId="formInlineName">
+      <FormControl type="number" placeholder="value" />
+    </FormGroup>
+
+  </Form>
+)
+
+const AnswerType = ({
+  // some props
+  id,
+  onChange,
+}) => (
+  <FormGroup controlId={"type-" + id}>
+    <Col componentClass={ControlLabel} sm={2}>
+      Type
+    </Col>
+    <Col componentClass={ControlLabel} sm={10} >
+      <FormControl
+        componentClass="select"
+        onChange={(evt) => onChange(evt.target.value)}
+        // value={this.}
+      >
+        <option value="text">Text</option>
+        <option value="number">Number</option>
+        <option value="radio">Yes/No</option>
+      </FormControl>
+    </Col>
+  </FormGroup>
+
+);
+
+
+const Question = ({
+  // props
+  question,
+  onAnswerTypeChange,
+}) => (
+  <div>
+      <Well bsSize="large">
+        {/*TODO Add condition t/f to   */}
+        {question.condition ? <Condition/> : null}
+        <Form horizontal>
           <FormGroup
-            controlId={"question-" + this.props.id} //make controlId unique **TODO**
+            controlId={"question-" + question.id}
           >
-            <ControlLabel>Question</ControlLabel>
+            <Col componentClass={ControlLabel} sm={2}>
+              Question
+            </Col>
+            <Col  componentClass={ControlLabel} sm={10}>
               <FormControl
                 type="text"
                 inputRef={ref => { this.input = ref; }}
-                // will update the value by dispatch  **TODO** FIXME **use event handler
                 onChange=  {() => {
                     store.dispatch({
                       type:"CHANGE_TEXT",
-                      id:this.props.id,
+                      id: question.id,
                       text:this.input.value,
                     });
                   }}
                 // value={this.state.value}
                 placeholder="Enter question"
               />
-          </FormGroup>
-          <FormGroup controlId={"type-" + this.props.id}>
-            <ControlLabel>Type</ControlLabel>
-              <FormControl
-                componentClass="select"
-                placeholder="select"
-                inputRef={ref => { this.input = ref; }}
-                onChange={() => {
-                  store.dispatch({
-                    type: "CHANGE_TYPE",
-                    id: this.props.id,
-                    answerType: this.input.value, //**TODO** FIXME, needs selected option value **use event handler
-                  })
-                }}
-              >
-                <option value="text">Text</option>
-                <option value="number">Number</option>
-                <option value="radio">Yes/No</option>
-              </FormControl>
+            </Col>
           </FormGroup>
 
-            <ButtonToolbar>
-                <Button bsStyle="primary" bsSize="sm" >
-                  Sub-Input
-                </Button>
+          <AnswerType
+            id={question.id}
+            // pass question.answerType to the value in the FormControl
+            onChange={ (value) => onAnswerTypeChange(question.id, value) }/>
 
-                <Button bsStyle="danger" bsSize="sm" >
-                  Delete
-                </Button>
-            </ButtonToolbar>
-        </form>
-      </div>
-    )
-  }
-}
+          <ButtonToolbar>
+              <Button bsStyle="primary" bsSize="sm" >
+                Sub-Input
+              </Button>
+              {/*  TODO will need dispatch at some point */}
+              <Button bsStyle="danger" bsSize="sm" >
+                Delete
+              </Button>
+          </ButtonToolbar>
+        </Form>
+    </Well>
+  </div>
+);
 
+
+const Questions = ({
+  questions,
+  onAnswerTypeChange,
+}) => (
+  <div>
+    <ul style={{listStyle: 'none'}}>
+      {questions.map(question =>
+      <li key={question.id}>
+        <Question
+          question={question}
+          onAnswerTypeChange={onAnswerTypeChange}/>
+      </li>
+      )}
+    </ul>
+  </div>
+);
 
 class FormBuilder extends React.Component {
 
   // use map to get each q from state and create each form
   // will have navs here
   render() {
-    console.log(store.getState());
+    console.log(store.getState()); //TODO REMOVE ME EVENTUALLY
+    const questions = this.props.questions
+
     return (
       <div>
         <div>
-        <PageHeader>
-          Form Builder 0.1
-        </PageHeader>
+          <PageHeader>
+            Form Builder 0.1
+          </PageHeader>
         </div>
 
-        <ul>
-          {this.props.questions.map(qu =>
-          <li key={qu.id}>
-            <Question id={qu.id}/>
-          </li>
-          )}
-        </ul>
+          <Questions
+            questions={questions}
+            onAnswerTypeChange={ (id, answerType) => {
+              store.dispatch({
+                type: "CHANGE_TYPE",
+                id,
+                answerType,
+              })
+            }}
+          />
 
         <div>
-        <Button
-          bsStyle="primary"
-          bsSize="large"
-          active
-          onClick={()=> {
-            store.dispatch({
-              type: "ADD_QUESTION",
-              text: "newQuestion",
-              answerType: "text" //could be "radio" or "number"
-            })
-          }}
-          >
-          Add Input
-        </Button>
+          <Button
+            bsStyle="primary"
+            bsSize="large"
+            active
+            onClick={()=> {
+              store.dispatch({
+                type: "ADD_QUESTION",
+                text: "newQuestion",
+                answerType: "text",
+                condition: null, //could be "radio" or "number"
+              })
+            }}
+            >
+            Add Input
+          </Button>
         </div>
-
       </div>
     );
   }
@@ -197,7 +264,8 @@ class FormBuilder extends React.Component {
 const render = () => {
   ReactDOM.render(
     <FormBuilder
-    questions={store.getState().questions}/>,
+    questions={store.getState().questions}
+    />,
     document.getElementById('root'));
 }
 
